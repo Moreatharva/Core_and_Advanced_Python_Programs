83de7dedcd7f1712ad2fda3e89e87a2c29e7ee73
Atharva More <144086264+Moreatharva@users.noreply.github.com>
2025-02-13 13:28:35 +0530

    Add files via upload

---
+++ Day 5_Assignment_1.py
@@ -0,0 +1,24 @@
+# Single Line Comment : Declare a div() function with two parameters. Then call the function and pass two numbers and display their division.
+
+
+# Declare the div function 
+def div(a,b):
+
+# Return the division of a and b
+ answer = a/b
+ return answer
+
+# Call the function ans pass two numbers
+result=div(12,4)
+
+#Disply the result
+print( "The resullt od Division is:",result)
+
+
+
+""" OUTPUT
+
+The resullt od Division is: 3.0
+
+
+"""
---
+++ Day 5_Assignment_2.py
@@ -0,0 +1,20 @@
+# Single Line Comment: Declare a square() function with one parameter.Then call the function and pass one number and display the square of that number
+# Define the square function
+def square(x):
+    answer=x*x
+#Return the sqare of the number
+    return x * x
+
+# Call the function and pass a number
+result = square(54)
+
+# Display the result
+print("The square of the number is:", result)
+
+
+"""OUTPUT
+
+The square of the number is: 2916
+
+
+"""

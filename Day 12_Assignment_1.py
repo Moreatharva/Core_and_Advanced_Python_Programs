bae7fcecc89668372fe40eaa5aa63390e85328f9
Atharva More <144086264+Moreatharva@users.noreply.github.com>
2025-02-21 12:42:02 +0530

    Add files via upload

---
+++ Day 12_Assignment_1.py
@@ -0,0 +1,20 @@
+# Single Line Comment:Write a Python program to find the number of times 4 appears in the tuple. 
+
+# Define the tuple from user
+tuplex = (2, 4, 5, 6, 2, 3, 4, 4, 7)
+
+# Count occurrences of 4
+count_4 = tuplex.count(4)
+
+# Print the result
+print("The number 4 Appears in Tulpe is:",count_4)
+
+
+""" OUTPUT
+
+
+The number 4 Appears in Tulpe is: 3
+
+"""
+
+
---
+++ Day 12_Assignment_2.py
@@ -0,0 +1,14 @@
+#Singele Line Comment:Write a Python program to convert a list to a tuple. 
+# Define the list
+listx = [5, 10, 7, 4, 15, 3]
+
+# Convert the list to a tuple
+tuplex = tuple(listx)
+
+# Print the result
+print("Converted List into Tuple:",tuplex)
+""" OUTPUT
+
+Converted List into Tuple: [5, 10, 7, 4, 15, 3]
+
+"""
